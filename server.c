80d7702ef78004eb7961e3ceabc55f9572f061e4
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-09-19 14:52:33 +0200

    Fixed response

--- server.c
+++ server.c
@@ -80,9 +80,8 @@ int main(int argc, char *argv[]) {
     SHA256_Update(&sha256, query, 8);
     SHA256_Final(hashTest, &sha256);
     printf("Are they equal? %d", memcmp(hash, hashTest, 32)==0);
-    printf("goofy print");
 
-    write(newSocketDescriptor, swapEndianness(query), 8);
+    write(newSocketDescriptor, swapEndianness(&query, 8), 8);
 
     return 0;
 }
