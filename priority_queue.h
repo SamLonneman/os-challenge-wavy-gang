e99f9c2db0e566ff8ce892a7f42ac8d1361b9dc5
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-11-16 15:42:50 +0100

    More cleaning

--- priority_queue.h
+++ priority_queue.h
@@ -17,55 +17,55 @@ void init_priority_queue(priority_queue_t* pq) {
     pq->capacity = 100;
 }
 
-// Internal function to swap two requests in a priority queue
+// Helper function to swap two requests in a priority queue
 void swap(priority_queue_t* pq, int i, int j) {
     request_t temp = pq->arr[i];
     pq->arr[i] = pq->arr[j];
     pq->arr[j] = temp;
 }
 
-// Internal function to heapify down a request within a priority queue
-void heapify_down(priority_queue_t* pq, int i) {
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int largest = i;
-    if (left < pq->size && compare_requests(&pq->arr[left], &pq->arr[largest]) > 0)
-        largest = left;
-    if (right < pq->size && compare_requests(&pq->arr[right], &pq->arr[largest]) > 0)
-        largest = right;
-    if (largest != i) {
-        swap(pq, i, largest);
-        heapify_down(pq, largest);
-    }
-}
-
-// Internal function to heapify up a request within a priority queue
-void heapify_up(priority_queue_t* pq, int i) {
-    if (i == 0)
-        return;
-    int parent = (i - 1) / 2;
-    if (compare_requests(&pq->arr[i], &pq->arr[parent]) > 0) {
-        swap(pq, i, parent);
-        heapify_up(pq, parent);
-    }
-}
-
-// External function to insert a request into a priority queue and dynamically resize if necessary
+// External function to insert a request into a priority queue
 void insert(priority_queue_t* pq, request_t* request) {
+    // Resize if full
     if (pq->size == pq->capacity) {
         pq->capacity *= 2;
         pq->arr = realloc(pq->arr, sizeof(request_t) * pq->capacity);
     }
-    pq->arr[pq->size] = *request;
-    heapify_up(pq, pq->size++);
+    // Insert request in last spot and bubble up
+    pq->arr[pq->size++] = *request;
+    int i = pq->size - 1;
+    int parent = (i - 1) / 2;
+    while (i > 0 && compare_requests(&pq->arr[i], &pq->arr[parent]) > 0) {
+        swap(pq, i, parent);
+        i = parent;
+    }
 }
 
 // External function to retrieve and delete the highest priority request from a priority queue
 request_t extract(priority_queue_t* pq) {
+    // Save the highest priority request for return
     request_t request = pq->arr[0];
-    pq->arr[0] = pq->arr[--pq->size];
-    heapify_down(pq, 0);
+    // Move the last request to the top
+    pq->arr[0] = pq->arr[pq->size - 1];
+    pq->size--;
+    // Bubble down
+    int i = 0;
+    while (i < pq->size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int max = i;
+        if (left < pq->size && compare_requests(&pq->arr[left], &pq->arr[max]) > 0)
+            max = left;
+        if (right < pq->size && compare_requests(&pq->arr[right], &pq->arr[max]) > 0)
+            max = right;
+        if (max == i)
+            break;
+        swap(pq, i, max);
+        i = max;
+    }
+    // Return saved request
     return request;
 }
+
 
 #endif // OS_CHALLENGE_WAVY_GANG_PRIORITY_QUEUE_H
