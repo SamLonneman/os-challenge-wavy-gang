e599ac1e579c7807af23fea9b7d3f2cc96d24392
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2022-11-17 14:41:11 +0100

    Combined solutions 4v3 and 5

--- server.c
+++ server.c
@@ -11,27 +11,29 @@
 #include <string.h>
 #include <unistd.h>
 #include "messages.h"
+#include "cache.h"
 #include "priority_queue.h"
 
-// Declare semaphore and mutex
+// Declare semaphore and mutex for preventing race conditions
 sem_t requests_in_queue;
 pthread_mutex_t lock;
 
-// Takes in pointer to priority queue
-void* worker_thread(void *pq) {
+// Define shared cache and shared request priority queue
+cache_t cache;
+priority_queue_t pq;
 
-    // Repeatedly take requests from queue and process them
+// A worker thread repeatedly handles requests from the priority queue
+void* worker_thread() {
     while (1) {
-
-        // Block while queue is empty
+        // Block while priority queue is empty
         sem_wait(&requests_in_queue);
 
         // Extract the request with the highest priority
         pthread_mutex_lock(&lock);
-        request_t request = extract(pq);
+        request_t request = extract(&pq);
         pthread_mutex_unlock(&lock);
 
-        // Search for key in given range corresponding to given hash
+        // Reverse hash to find the key which produced the given hash
         uint8_t calculatedHash[32];
         uint64_t key;
         for (key = request.start; key < request.end; key++) {
@@ -44,6 +46,11 @@ void* worker_thread(void *pq) {
         key = be64toh(key);
         write(request.newsockfd, &key, 8);
         close(request.newsockfd);
+
+        // Insert the (hash, key) pair into the cache
+        pthread_mutex_lock(&lock);
+        cache_insert(&cache, calculatedHash, key);
+        pthread_mutex_unlock(&lock);
     }
 }
 
@@ -52,6 +59,10 @@ int main(int argc, char *argv[]) {
 
     // Initialize Mutex
     pthread_mutex_init(&lock, NULL);
+
+    // Initialize shared request priority queue and cache
+    init_priority_queue(&pq);
+    init_cache(&cache);
 
     // Create socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -82,17 +93,13 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in cli_addr;
     int clilen = sizeof(cli_addr);
 
-    // Initialize the priority queue
-    priority_queue_t pq;
-    init_priority_queue(&pq);
-
     // Initialize thread pool
     pthread_t threads[4];
     for (int i = 0; i < 4; i++)
-        pthread_create(&threads[i], NULL, worker_thread, &pq);
+        pthread_create(&threads[i], NULL, worker_thread, NULL);
 
     // Initialize request counter
-    int c = 0;
+    int requestCounter = 0;
 
     // Continuously accept requests to populate the priority queue
     while (1) {
@@ -103,6 +110,7 @@ int main(int argc, char *argv[]) {
             perror("ERROR on accept");
             exit(1);
         }
+        requestCounter++;
 
         // Read in request through new connection
         uint8_t buffer[PACKET_REQUEST_SIZE];
@@ -122,11 +130,21 @@ int main(int argc, char *argv[]) {
         start = htobe64(start);
         end = htobe64(end);
 
-        // Create request object
-        request_t request;
-        init_request(&request, hash, start, end, p, newsockfd, ++c);
+        // If the hash is in the cache, immediately send the cached key back to the client and continue.
+        pthread_mutex_lock(&lock);
+        pair_t *cache_result = cache_search(&cache, hash);
+        pthread_mutex_unlock(&lock);
+        if (cache_result) {
+            write(newsockfd, &cache_result->key, 8);
+            close(newsockfd);
+            continue;
+        }
 
-        // Insert request into priority queue
+        // Otherwise, create request object
+        request_t request;
+        init_request(&request, hash, start, end, p, newsockfd, requestCounter);
+
+        // Insert request into priority queue for worker threads to process
         pthread_mutex_lock(&lock);
         insert(&pq, &request);
         pthread_mutex_unlock(&lock);
