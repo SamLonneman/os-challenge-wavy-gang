6b718b04016d3a3db16f48511548c4f39945236e
Alana Vanzo <63185241+Alanavanzo@users.noreply.github.com>
2022-11-21 22:31:12 +0100

    removed print statement

--- server.c
+++ server.c
@@ -141,7 +141,6 @@ int main(int argc, char *argv[]) {
                     }
 
                     key = be64toh(key);
-                    printf("Request %d \n", p);
                     write(newSockFd, &key, 8);
                     close(newSockFd);
                     j = j - 1;
